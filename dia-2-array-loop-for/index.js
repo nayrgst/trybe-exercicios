84d174f6a98cc15727d378a469c5736bc028d897
nayrgst <ryan567.pr@gmail.com>
2021-11-29 19:31:04 -0300

    resoluçao do exercicio 5

--- dia-2-array-loop-for/index.js
+++ dia-2-array-loop-for/index.js
@@ -1,14 +1,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let total = 0;
+//ajuda para resolver essa questão foi em https://www.horadecodar.com.br/2021/06/29/encontrar-maior-e-menor-numero-de-array-em-javascript/
+let resultado = Math.max(...numbers);
+console.log(resultado)
 
-for (let index = 0; index < numbers.length; index += 1) {
-    total += numbers[index];
-}
 
-let resultado = total / numbers.length;
 
-if ( resultado >= 20) {
-    console.log ("Valor maior que 20")
-} else {
-    console.log ("Valor menor ou igual a 20");
-}
+
+// let total = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     total += numbers[index];
+// }
+
+// let resultado = total / numbers.length;
+
+// if ( resultado >= 20) {
+//     console.log ("Valor maior que 20")
+// } else {
+//     console.log ("Valor menor ou igual a 20");
+// }
