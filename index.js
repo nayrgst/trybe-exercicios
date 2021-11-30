259806e2967b5e83b82868d998fad12d7a8f7ef9
nayrgst <ryan567.pr@gmail.com>
2021-11-30 19:04:56 -0300

    resoluçao da parte 1 do exercicio

---
+++ index.js
@@ -0,0 +1,10 @@
+let n = 4;
+let asteriscos = "*"
+let linha = ''
+
+for (let index = 0; index < n; index += 1) {
+    linha = linha + asteriscos;
+}
+ for (index = 0; index < n; index += 1) {
+     console.log (linha)
+ }
