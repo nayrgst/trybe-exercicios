f39cd7471e3d469032e059a8fd006830bc06ecb7
nayrgst <ryan567.pr@gmail.com>
2021-11-30 19:27:41 -0300

    resoluçao parte 3 do exercicio

--- index.js
+++ index.js
@@ -9,11 +9,29 @@
 //      console.log (linha)
 //  }
 
-let n = 4;
-let asteriscos = "*";
-let linha = '';
+// let n = 4;
+// let asteriscos = "*";
+// let linha = '';
 
-for (let index = 0; index <= n; index += 1) {
-    console.log (linha)
-    linha = linha + asteriscos;
+// for (let index = 0; index <= n; index += 1) {
+//     console.log (linha)
+//     linha = linha + asteriscos;
+// }
+
+let n = 4;
+let asteriscos = "*"
+let linha = ''
+let espaço = n
+
+for (let index = 0; index < n; index += 1) {
+    for (i  = 0; i <= n; i += 1) {
+    if (i < espaço) {
+        linha = linha + ' ';
+    } else {
+        linha = linha + asteriscos;
+    }
+}
+    console.log(linha)
+    linha = '';
+    espaço -= 1;
 }
