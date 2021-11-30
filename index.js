6528e7a227015810beca897961636e86871d01dc
nayrgst <ryan567.pr@gmail.com>
2021-11-30 20:18:27 -0300

    resoluçao dos exercicios

--- index.js
+++ index.js
@@ -18,20 +18,42 @@
 //     linha = linha + asteriscos;
 // }
 
-let n = 4;
-let asteriscos = "*"
-let linha = ''
-let espaço = n
+// let n = 4;
+// let asteriscos = "*"
+// let linha = ''
+// let espaço = n
 
-for (let index = 0; index < n; index += 1) {
+// for (let index = 0; index < n; index += 1) {
+//     for (i  = 0; i <= n; i += 1) {
+//     if (i < espaço) {
+//         linha = linha + ' ';
+//     } else {
+//         linha = linha + asteriscos;
+//     }
+// }
+//     console.log(linha)
+//     linha = '';
+//     espaço -= 1;
+// }
+
+let n = 11;
+let asteriscos = "*";
+let linha = '';
+let espaço = n;
+let meio = (n + 1 )/ 2;
+let left = meio;
+let right = meio; 
+
+for (let index = 0; index <= meio; index += 1) {
     for (i  = 0; i <= n; i += 1) {
-    if (i < espaço) {
-        linha = linha + ' ';
-    } else {
+    if (i > left && i < right) {
         linha = linha + asteriscos;
+    } else {
+        linha = linha + " ";
     }
 }
     console.log(linha)
     linha = '';
-    espaço -= 1;
+    right += 1;
+    left -= 1;
 }
