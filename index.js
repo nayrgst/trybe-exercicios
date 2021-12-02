996f8769772001b6a773319330368f4d3f95b7cd
nayrgst <ryan567.pr@gmail.com>
2021-12-02 10:59:23 -0300

    Resoluçao da quarta parte do exercicio 4.4

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'sim'
 };
-    for( let all in info){
-        console.log (all)
+  
+    for (let all in info) {
+        console.log (info[all])
     }
+
+// for( let all in info){
+//     console.log (all)
+// }
 // console.log ('Bem-vinda, ' + info.personagem);
