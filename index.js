fe34eff041dcb2782ccb7beb8840693c03a746af
nayrgst <ryan567.pr@gmail.com>
2021-12-02 11:53:29 -0300

    Resoluçao do exercicio 4.4

--- index.js
+++ index.js
@@ -37,5 +37,16 @@ let leitor = {
     },
   ],
 };
+  leitor.livrosFavoritos.push(
+    {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+}
+  )
+    console.log ( leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos')
 
-console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'" );
+
+// console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'" );
+
+
