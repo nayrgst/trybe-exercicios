8d5fefd1ffe38849ef05ff29ede4e98c0dd63525
nayrgst <ryan567.pr@gmail.com>
2021-12-02 11:21:20 -0300

    Resoluçao da quinta parte do exercicio 4.4

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'sim'
 };
-  
-    for (let all in info) {
-        console.log (info[all])
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem:  "Christmas on Bear Mountain, Dell's Four Color Comics #178'",
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim'
+}  
+    for(let all in info) {
+        console.log (info[all] + ' e ' + info2[all]); 
     }
+
+
+
+    // for (let all in info) {
+    //     console.log (info[all])
+    // }
 
 // for( let all in info){
 //     console.log (all)
