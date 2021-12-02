f515253f0f9415051096a1a7bf7d9d641ac1f2ec
nayrgst <ryan567.pr@gmail.com>
2021-12-02 11:31:12 -0300

    Resoluçao da sexta  parte do exercicio 4.4

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'sim'
-};
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem:  "Christmas on Bear Mountain, Dell's Four Color Comics #178'",
-    nota: 'O último MacPatinhas',
-    recorrente: 'sim'
-}  
-    for(let all in info) {
-        console.log (info[all] + ' e ' + info2[all]); 
-    }
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'sim'
+// };
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem:  "Christmas on Bear Mountain, Dell's Four Color Comics #178'",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'sim'
+// }  
+//     for(let all in info) {
+//         console.log (info[all] + ' e ' + info2[all]) 
+//     }
 
 
 
@@ -24,3 +24,18 @@ let info2 = {
 //     console.log (all)
 // }
 // console.log ('Bem-vinda, ' + info.personagem);
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + "'" + leitor.livrosFavoritos[0].titulo + "'" );
