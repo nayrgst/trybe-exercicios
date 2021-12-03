7db59ce1af0c967c7c678ccdd614fc5d56a47731
nayrgst <ryan567.pr@gmail.com>
2021-12-03 18:55:23 -0300

    resolvido a segunda parte do exercicio

--- dia-1-DOM/script.js
+++ dia-1-DOM/script.js
@@ -1,4 +1,4 @@
 document.getElementById('descricao').innerText = "Se Deus quiser e tudo der certo, vou está formado e trabalhando na empresa que me deu gás para querer entrar na Trybe";
 
-
+document.getElementsByClassName('main-content')[0].style.backgroundColor = ('rgb(76,164,109');
 
