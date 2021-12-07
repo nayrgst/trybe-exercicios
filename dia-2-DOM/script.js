01c003d04589019cbb027a0dfb8f70b924248072
nayrgst <ryan567.pr@gmail.com>
2021-12-06 22:47:01 -0300

    resoluçao da terceira parte do exercicio

--- dia-2-DOM/script.js
+++ dia-2-DOM/script.js
@@ -1,20 +1,24 @@
-        let container = document.querySelector('body');
+  let container = document.querySelector('body');
 
-        function createChild(element) {
-          let newChild = document.createElement('h1');
-          newChild.innerText = element;
-          return newChild;
-        }
+  function createChild(element) {
+  let newChild = document.createElement('h1');
+  newChild.innerText = element;
+    return newChild;
+  };
 
-        function createChildMain(classe) {
-          let newChild = document.createElement('main');
-          newChild.className = classe;
-          return newChild;
-        }
-
+    function createChildMain(classe) {
+    let newChild = document.createElement('main');
+    newChild.className = classe;
+    return newChild;
+    };
     
 
+  container.appendChild(createChild('Exercício 5.2 - JavaScript DOM'));
+  container.appendChild(createChildMain('main-content'));
 
-        container.appendChild(createChild('Exercício 5.2 - JavaScript DOM'));
-        container.appendChild(createChildMain('main-content'));
- 
+
+
+    let container2 =  document.getElementsByTagName('main');
+    let newChild = document.createElement('section');
+      newChild.className = 'center-content';
+    container2[0].appendChild(newChild);
