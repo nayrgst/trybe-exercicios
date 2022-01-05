4070778e6804a784daee9201d6aa7e57de2fa4d3
nayrgst <ryan567.pr@gmail.com>
2022-01-05 15:02:42 -0300

    feito botão input data

--- dia-1-html-css-forms/script.js
+++ dia-1-html-css-forms/script.js
@@ -1,6 +1,9 @@
 const criarOp = document.querySelector('#estados')
+const mes = '12'
+const dia = '31'
 const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará','Espírito', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo	', 'Sergipe	', 'Tocantins'
 ];
+
 
 function criarOps() {
   for (let index = 0; index < estados.length; index += 1){
@@ -10,8 +13,16 @@ function criarOps() {
   };
 };
 criarOps();
+
+function data() {
+  if (dia > 0 && dia <= 30) {
+  } else if (mes > 0 && mes <= 12){ 
+  } else {
+    alert('Data inválida');
+  };
+};
 	
-	
+	data();
 	
 
 	
