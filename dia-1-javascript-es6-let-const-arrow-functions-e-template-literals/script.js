a09b14639da027fb7821726b6fc45854efe08893
nayrgst <ryan567.pr@gmail.com>
2022-01-12 13:30:29 -0300

    Resoluçao do exercicio 2 da parte 2

--- dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -11,8 +11,7 @@
 
 //   testingScope(true);
 
-// parte 2
-
+//------------------------------------------------- parte 2 -----------------------------------------------------------------------
 // const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // const crescente = () => {
@@ -29,14 +28,27 @@
 
 // console.log(`Os números ${ordem} e encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉 s
 
+// const factorial = n => {
+//   let f = 1;
+//   for( let index = 1; index <= n; index += 1){
+//     f *= index;
+//   }
+//   return f;
+// }
+// console.log(factorial(3));
 
-const factorial = n => {
-  let f = 1;
-  for( let index = 1; index <= n; index += 1){
-    f *= index;
+const maior = valor => {
+  let separar = valor.split(' ');
+  let resultado = '';
+  let total = 0;
+
+  for(let index of separar){
+    if(index.length > total){
+      total = index.length
+      resultado = index;
+    }
   }
-  return f;
-}
-console.log(factorial(3));
+  return resultado;
+};
 
- 
+console.log(maior('Antonio foi no banheiro e não sabemos o que aconteceu, depois descobrimos que ele ficou preso em um paralelepipedo'));
